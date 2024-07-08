f749fe6b802e7ead9caeafcda48f2e96b70c54af
hwoldeamanuel <habtamuwh@gmail.com>
2024-07-08 23:11:24 +0300

    second commit2

--- home/urls.py
+++ home/urls.py
@@ -3,13 +3,13 @@ from django.urls import path, include
 
 from django.contrib.auth.views import LogoutView
 from . import views
-
+#jfkldsjfds
 
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     
-    path('', views.home, name='home'),
+    path('', views.home,  name='home'),
    
    
     
